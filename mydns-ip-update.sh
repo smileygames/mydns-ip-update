903d9b346c8c8d10e4476ccaeeadcadb2a88b1f1
smileygames <134200591+smileygames@users.noreply.github.com>
2023-05-21 19:53:55 +0900

    新規
    正式版

---
+++ mydns-ip-update.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# update IP address
+#
+# MyDNS
+
+# Config File ロード
+FILE_DIR="/usr/local/etc/"
+source "${FILE_DIR}mydns-ip-update.conf"
+
+mydns_update() {
+# ipv4用
+  if [ "$IPV4" = on ]; then
+    curl -s -u $MYDNS_IP:$MYDNS_PASS https://ipv4.mydns.jp/login.html
+  fi
+# ipv6用
+  if [ "$IPV6" = on ]; then
+    curl -s -u $MYDNS_IP:$MYDNS_PASS https://ipv6.mydns.jp/login.html
+  fi
+}
+
+mydns_update
+while true;do
+  echo "time = $UPDATE_TIME"  # debug
+done
