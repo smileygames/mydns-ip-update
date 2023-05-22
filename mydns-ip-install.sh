4f14cfa11de6a32cab2aeb9b317d3a3e3c483180
smile <134200591+smileygames@users.noreply.github.com>
2023-05-22 14:11:25 +0900

    Add files via upload

--- mydns-ip-install.sh
+++ mydns-ip-install.sh
@@ -10,16 +10,16 @@ sudo chown root:root /usr/local/etc/mydns-ip-update.conf
 sudo chmod 644 /usr/local/etc/mydns-ip-update.conf
 
 
-sudo wget -P /usr/bin/mydns-ip-update.sh https://github.com/smileygames/mydns-ip-update/releases/download/v1.01/mydns-ip-update.sh
+sudo wget -P /usr/bin https://github.com/smileygames/mydns-ip-update/releases/download/v1.01/mydns-ip-update.sh
 sudo chown root:root /usr/bin/mydns-ip-update.sh
 sudo chmod 755 /usr/bin/mydns-ip-update.sh
 
-sudo wget -P /etc/systemd/system/mydns-ip-update.service https://github.com/smileygames/mydns-ip-update/releases/download/v1.01/mydns-ip-update.service
+sudo wget -P /etc/systemd/system https://github.com/smileygames/mydns-ip-update/releases/download/v1.01/mydns-ip-update.service
 
-sudo wget -P /usr/bin/mydns-ip-change.sh https://github.com/smileygames/mydns-ip-update/releases/download/v1.01/mydns-ip-change.sh
+sudo wget -P /usr/bin https://github.com/smileygames/mydns-ip-update/releases/download/v1.01/mydns-ip-change.sh
 sudo chown root:root /usr/bin/mydns-ip-update.sh
 sudo chmod 755 /usr/bin/mydns-ip-update.sh
 
-sudo wget -P /etc/systemd/system/mydns-ip-change.service https://github.com/smileygames/mydns-ip-update/releases/download/v1.01/mydns-ip-change.service
+sudo wget -P /etc/systemd/system https://github.com/smileygames/mydns-ip-update/releases/download/v1.01/mydns-ip-change.service
 
 sudo systemctl daemon-reload
