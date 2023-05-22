e949e5cbb914f7945437e25a419ad9a56c43ef32
smile <134200591+smileygames@users.noreply.github.com>
2023-05-23 02:29:58 +0900

    for文の条件文の修正
    可読性を上げるため

--- mydns-ip-update.sh
+++ mydns-ip-update.sh
@@ -10,21 +10,21 @@ source "${FILE_DIR}mydns-ip-update.conf"
 
 mydns_update() {
 # ipv4用
-  if [ "$IPV4" = on ]; then
-    for ((i=0 ; i<${#MYDNS_IP[@]}; i++)) do
-      curl -s -u ${MYDNS_IP[i]}:${MYDNS_PASS[i]} https://ipv4.mydns.jp/login.html
-    done
-  fi
+    if [ "$IPV4" = on ]; then
+        for (( i = 0 ; i < ${#MYDNS_IP[@]} ; i++ )) do
+            curl -s -u ${MYDNS_IP[i]}:${MYDNS_PASS[i]} https://ipv4.mydns.jp/login.html
+        done
+    fi
 # ipv6用
-  if [ "$IPV6" = on ]; then
-    for ((i=0 ; i<${#MYDNS_IP[@]}; i++)) do
-      curl -s -u ${MYDNS_IP[i]}:${MYDNS_PASS[i]} https://ipv6.mydns.jp/login.html
-    done
-  fi
+    if [ "$IPV6" = on ]; then
+        for (( i = 0 ; i < ${#MYDNS_IP[@]} ; i++ )) do
+            curl -s -u ${MYDNS_IP[i]}:${MYDNS_PASS[i]} https://ipv6.mydns.jp/login.html
+        done
+    fi
 }
 
 # 実行スクリプト（タイマー処理）
 sleep 5m;mydns_update
 while true;do
-  sleep $UPDATE_TIME;mydns_update
+    sleep $UPDATE_TIME;mydns_update
 done
