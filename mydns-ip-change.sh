84f5f0732baf4bcff8ca1f332f1115e0ca645928
smile <134200591+smileygames@users.noreply.github.com>
2023-05-23 01:16:11 +0900

    自分なりにオブジェクト化してみた。
    正しいかはわからない。

--- mydns-ip-change.sh
+++ mydns-ip-change.sh
@@ -13,32 +13,24 @@ mydns_change() {
     if [ "$IPV4" = on ] && [ "$IPV4_DDNS" = on ]; then
         IP_NEW=$(curl -s ifconfig.io -4)
         if [ $IP_NEW != "" ]; then
-            multi_domain_ipv4
+            multi_domain "https://ipv4.mydns.jp/login.html"
         fi
     fi
 # ipv6
     if [ "$IPV6" = on ] && [ "$IPV6_DDNS" = on ]; then
-        IP6_NEW=$(curl -s ifconfig.io -6)
-        if [ $IP6_NEW != "" ]; then
-            multi_domain_ipv6
+        IP_NEW=$(curl -s ifconfig.io -6)
+        if [ $IP_NEW != "" ]; then
+            multi_domain "https://ipv6.mydns.jp/login.html"
         fi
     fi
 }
 
-multi_domain_ipv4() {
+# 引数としてLogin URLをもらう [$1]
+multi_domain() {
     for (( i = 0 ; i < ${#MYDNS_IP[@]} ; i++ )) do
         IP_OLD=$(dig "${MY_DOMAIN[i]}" A +short)
         if [[ $IP_NEW != $IP_OLD ]]; then
-            curl -s -u ${MYDNS_IP[i]}:${MYDNS_PASS[i]} https://ipv4.mydns.jp/login.html
-        fi
-    done
-}
-
-multi_domain_ipv6() {
-    for (( i = 0 ; i < ${#MYDNS_IP[@]} ; i++ )) do
-        IP6_OLD=$(dig "${MY_DOMAIN[i]}" AAAA +short)
-        if [[ $IP6_NEW != $IP6_OLD ]]; then
-            curl -s -u ${MYDNS_IP[i]}:${MYDNS_PASS[i]} https://ipv6.mydns.jp/login.html
+            curl -s -u ${MYDNS_IP[i]}:${MYDNS_PASS[i]} $1
         fi
     done
 }
