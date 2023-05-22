c14c1bb9ee6848e4d6bb86c0a8bb0f7bba84eb76
smile <134200591+smileygames@users.noreply.github.com>
2023-05-23 01:53:45 +0900

    バグ修正

--- mydns-ip-change.sh
+++ mydns-ip-change.sh
@@ -13,24 +13,24 @@ mydns_change() {
     if [ "$IPV4" = on ] && [ "$IPV4_DDNS" = on ]; then
         IP_NEW=$(curl -s ifconfig.io -4)
         if [ $IP_NEW != "" ]; then
-            multi_domain "https://ipv4.mydns.jp/login.html"
+            multi_domain "A" "https://ipv4.mydns.jp/login.html"
         fi
     fi
 # ipv6
     if [ "$IPV6" = on ] && [ "$IPV6_DDNS" = on ]; then
         IP_NEW=$(curl -s ifconfig.io -6)
         if [ $IP_NEW != "" ]; then
-            multi_domain "https://ipv6.mydns.jp/login.html"
+            multi_domain "AAAA" "https://ipv6.mydns.jp/login.html"
         fi
     fi
 }
 
-# 引数としてLogin URLをもらう [$1]
+# 引数としてレコードとLogin URLをもらう $1=レコード $2=URL
 multi_domain() {
     for (( i = 0 ; i < ${#MYDNS_IP[@]} ; i++ )) do
-        IP_OLD=$(dig "${MY_DOMAIN[i]}" A +short)
+        IP_OLD=$(dig "${MY_DOMAIN[i]}" $1 +short)
         if [[ $IP_NEW != $IP_OLD ]]; then
-            curl -s -u ${MYDNS_IP[i]}:${MYDNS_PASS[i]} $1
+            curl -s -u ${MYDNS_IP[i]}:${MYDNS_PASS[i]} $2
         fi
     done
 }
