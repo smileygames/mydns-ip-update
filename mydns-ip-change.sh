6282247135f400cfa5d92367cfaf69f9fc2fca5d
smile <134200591+smileygames@users.noreply.github.com>
2023-05-23 17:30:08 +0900

    ユーザー認証エラー＆認証タイムアウト時の処理を追加
    ユーザー認証に失敗すると、再度パスワードを聞いてくる？
    そのまま永遠と入力を待ってしまうので、スタックしてしまう。
    その対処でタイムアウト処理を追加した。
    タイムアウトした場合、強制終了となる。

--- mydns-ip-change.sh
+++ mydns-ip-change.sh
@@ -13,26 +13,29 @@ mydns_change() {
     if [ "$IPV4" = on ] && [ "$IPV4_DDNS" = on ]; then
         IP_NEW=$(curl -s ifconfig.io -4)
         if [[ $IP_NEW != "" ]]; then
-            multi_domain "A" "https://ipv4.mydns.jp/login.html"
+            multi_domain_change "A" "https://ipv4.mydns.jp/login.html"
         fi
     fi
 # ipv6
     if [ "$IPV6" = on ] && [ "$IPV6_DDNS" = on ]; then
         IP_NEW=$(curl -s ifconfig.io -6)
         if [[ $IP_NEW != "" ]]; then
-            multi_domain "AAAA" "https://ipv6.mydns.jp/login.html"
+            multi_domain_change "AAAA" "https://ipv6.mydns.jp/login.html"
         fi
     fi
 }
 
 # 引数としてレコードとLogin URLをもらう $1=レコード $2=URL
 # IP_NEWはコールされる前に入れておくこと
-multi_domain() {
+multi_domain_change() {
     for (( i = 0 ; i < ${#MYDNS_IP[@]} ; i++ )) do
         IP_OLD=$(dig "${MY_DOMAIN[i]}" $1 +short)
-        if [[ $IP_OLD != "" ]]; then
-            if [[ $IP_NEW != $IP_OLD ]]; then
-                curl -s -u ${MYDNS_IP[i]}:${MYDNS_PASS[i]} $2
+        if [[ $IP_OLD != "" ]] && [[ $IP_NEW != $IP_OLD ]]; then
+            timeout 10 curl -su ${MYDNS_IP[i]}:${MYDNS_PASS[i]} $2
+            if [ $? != 0 ]; then 
+                # タイムアウト時の処理
+                echo "Time Out!"
+                exit
             fi
         fi
     done
--- mydns-ip-update.sh
+++ mydns-ip-update.sh
@@ -11,16 +11,24 @@ source "${FILE_DIR}mydns-ip-update.conf"
 mydns_update() {
 # ipv4用
     if [ "$IPV4" = on ]; then
-        for (( i = 0 ; i < ${#MYDNS_IP[@]} ; i++ )) do
-            curl -s -u ${MYDNS_IP[i]}:${MYDNS_PASS[i]} https://ipv4.mydns.jp/login.html
-        done
+        multi_domain_update https://ipv4.mydns.jp/login.html
     fi
 # ipv6用
     if [ "$IPV6" = on ]; then
-        for (( i = 0 ; i < ${#MYDNS_IP[@]} ; i++ )) do
-            curl -s -u ${MYDNS_IP[i]}:${MYDNS_PASS[i]} https://ipv6.mydns.jp/login.html
-        done
+        multi_domain_update "https://ipv6.mydns.jp/login.html"
     fi
+}
+
+# 引数としてLogin URLをもらう $1=URL
+multi_domain_update() {
+    for (( i = 0 ; i < ${#MYDNS_IP[@]} ; i++ )) do
+        timeout 10 curl -su ${MYDNS_IP[i]}:${MYDNS_PASS[i]} $1
+        if [ $? != 0 ]; then 
+            # タイムアウト時の処理
+            echo "Time Out!"
+            exit
+        fi
+    done
 }
 
 # 実行スクリプト（タイマー処理）
