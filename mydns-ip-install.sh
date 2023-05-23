08cb1a1e75dfdd060c7503e08d00ab26a2a4d750
smile <134200591+smileygames@users.noreply.github.com>
2023-05-23 12:39:34 +0900

    ユーザー権限でサービスを作成させる処理の変更
    ”>>”　リダイレクト処理？にsudoコマンドをつけることができないので、
    "| sudo tee -a"に置き換えた。

--- mydns-ip-install.sh
+++ mydns-ip-install.sh
@@ -18,38 +18,35 @@ sudo chmod 755 /usr/bin/mydns-ip-change.sh
 
 
 sudo rm -f /etc/systemd/system/mydns-ip-update.service
-touch ./mydns-ip-update.service
+sudo touch /etc/systemd/system/mydns-ip-update.service
 
-echo '[Unit]' >> ./mydns-ip-update.service
-echo 'Description=mydns-ip-update' >> ./mydns-ip-update.service
-echo '' >> ./mydns-ip-update.service
-echo '[Service]' >> ./mydns-ip-update.service
-echo 'Type=simple' >> ./mydns-ip-update.service
-echo 'ExecStart=/usr/bin/mydns-ip-update.sh' >> ./mydns-ip-update.service
-echo '' >> ./mydns-ip-update.service
-echo '[Install]' >> ./mydns-ip-update.service
-echo 'WantedBy=network-online.target' >> ./mydns-ip-update.service
+echo '[Unit]' | sudo tee -a /etc/systemd/system/mydns-ip-update.service
+echo 'Description=mydns-ip-update' | sudo tee -a /etc/systemd/system/mydns-ip-update.service
+echo '' | sudo tee -a /etc/systemd/system/mydns-ip-update.service
+echo '[Service]' | sudo tee -a /etc/systemd/system/mydns-ip-update.service
+echo 'Type=simple' | sudo tee -a /etc/systemd/system/mydns-ip-update.service
+echo 'ExecStart=/usr/bin/mydns-ip-update.sh' | sudo tee -a /etc/systemd/system/mydns-ip-update.service
+echo '' | sudo tee -a /etc/systemd/system/mydns-ip-update.service
+echo '[Install]' | sudo tee -a /etc/systemd/system/mydns-ip-update.service
+echo 'WantedBy=network-online.target' | sudo tee -a /etc/systemd/system/mydns-ip-update.service
 
-sudo mv ./mydns-ip-update.service /etc/systemd/system/mydns-ip-update.service
 sudo chown root:root /etc/systemd/system/mydns-ip-update.service
 sudo chmod 644 /etc/systemd/system/mydns-ip-update.service
 
 sudo rm -f /etc/systemd/system/mydns-ip-change.service
-touch ./mydns-ip-change.service
+sudo touch /etc/systemd/system/mydns-ip-change.service
 
-echo '[Unit]' >> ./mydns-ip-change.service
-echo 'Description=mydns-ip-change' >> ./mydns-ip-change.service
-echo '' >> ./mydns-ip-change.service
-echo '[Service]' >> ./mydns-ip-change.service
-echo 'Type=simple' >> ./mydns-ip-change.service
-echo 'ExecStart=/usr/bin/mydns-ip-change.sh' >> ./mydns-ip-change.service
-echo '' >> ./mydns-ip-change.service
-echo '[Install]' >> ./mydns-ip-change.service
-echo 'WantedBy=network-online.target' >> ./mydns-ip-change.service
+echo '[Unit]' | sudo tee -a /etc/systemd/system/mydns-ip-change.service
+echo 'Description=mydns-ip-change' | sudo tee -a /etc/systemd/system/mydns-ip-change.service
+echo '' | sudo tee -a /etc/systemd/system/mydns-ip-change.service
+echo '[Service]' | sudo tee -a /etc/systemd/system/mydns-ip-change.service
+echo 'Type=simple' | sudo tee -a /etc/systemd/system/mydns-ip-change.service
+echo 'ExecStart=/usr/bin/mydns-ip-change.sh' | sudo tee -a /etc/systemd/system/mydns-ip-change.service
+echo '' | sudo tee -a /etc/systemd/system/mydns-ip-change.service
+echo '[Install]' | sudo tee -a /etc/systemd/system/mydns-ip-change.service
+echo 'WantedBy=network-online.target' | sudo tee -a /etc/systemd/system/mydns-ip-change.service
 
-sudo mv ./mydns-ip-change.service /etc/systemd/system/mydns-ip-change.service
 sudo chown root:root /etc/systemd/system/mydns-ip-change.service
 sudo chmod 644 /etc/systemd/system/mydns-ip-change.service
-
 
 sudo systemctl daemon-reload
