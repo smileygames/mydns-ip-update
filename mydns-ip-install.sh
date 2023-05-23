af3d8c7e992f08bbd3e74d27496340db1c1e3233
smile <134200591+smileygames@users.noreply.github.com>
2023-05-23 13:50:38 +0900

    v1.03にするのを忘れていた。
    修正しました。

--- mydns-ip-install.sh
+++ mydns-ip-install.sh
@@ -4,15 +4,15 @@
 #
 # MyDNS 
 
-sudo wget -N -P /usr/local/etc https://github.com/smileygames/mydns-ip-update/releases/download/v1.02/mydns-ip-update.conf
+sudo wget -N -P /usr/local/etc https://github.com/smileygames/mydns-ip-update/releases/download/v1.03/mydns-ip-update.conf
 sudo chown root:root /usr/local/etc/mydns-ip-update.conf
 sudo chmod 600 /usr/local/etc/mydns-ip-update.conf
 
-sudo wget -N -P /usr/bin https://github.com/smileygames/mydns-ip-update/releases/download/v1.02/mydns-ip-update.sh
+sudo wget -N -P /usr/bin https://github.com/smileygames/mydns-ip-update/releases/download/v1.03/mydns-ip-update.sh
 sudo chown root:root /usr/bin/mydns-ip-update.sh
 sudo chmod 755 /usr/bin/mydns-ip-update.sh
 
-sudo wget -N -P /usr/bin https://github.com/smileygames/mydns-ip-update/releases/download/v1.02/mydns-ip-change.sh
+sudo wget -N -P /usr/bin https://github.com/smileygames/mydns-ip-update/releases/download/v1.03/mydns-ip-change.sh
 sudo chown root:root /usr/bin/mydns-ip-change.sh
 sudo chmod 755 /usr/bin/mydns-ip-change.sh
 
