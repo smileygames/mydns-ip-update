33132232df9426405274aea342460288b2f83b19
smile <134200591+smileygames@users.noreply.github.com>
2023-05-25 02:14:27 +0900

    dir変更の修正忘れ

--- mydns-ip/bin/mydns-ip-install.sh
+++ mydns-ip/bin/mydns-ip-install.sh
@@ -4,17 +4,17 @@
 #
 # MyDNS 
 
-sudo wget -NP /usr/local/mydns-ip https://github.com/smileygames/mydns-ip-update/releases/download/v1.06/mydns-ip-update.conf
-sudo chown root:root /usr/local/etc/mydns-ip-update.conf
-sudo chmod 600 /usr/local/etc/mydns-ip-update.conf
+sudo wget -NP /usr/local/mydns-ip https://github.com/smileygames/mydns-ip-update/releases/download/v1.06/mydns-ip.conf
+sudo chown root:root /usr/local/mydns-ip/mydns-ip.conf
+sudo chmod 600 /usr/local/mydns-ip/mydns-ip.conf
 
 sudo wget -NP /usr/local/mydns-ip/bin https://github.com/smileygames/mydns-ip-update/releases/download/v1.06/mydns-ip-update.sh
-sudo chown root:root /usr/bin/mydns-ip-update.sh
-sudo chmod 755 /usr/bin/mydns-ip-update.sh
+sudo chown root:root /usr/local/mydns-ip/bin/mydns-ip-update.sh
+sudo chmod 755 /usr/local/mydns-ip/bin/mydns-ip-update.sh
 
 sudo wget -NP /usr/local/mydns-ip/bin https://github.com/smileygames/mydns-ip-update/releases/download/v1.06/mydns-ip-change.sh
-sudo chown root:root /usr/bin/mydns-ip-change.sh
-sudo chmod 755 /usr/bin/mydns-ip-change.sh
+sudo chown root:root /usr/local/mydns-ip/bin/mydns-ip-change.sh
+sudo chmod 755 /usr/local/mydns-ip/bin/mydns-ip-change.sh
 
 cat << EOS | sudo tee /etc/systemd/system/mydns-ip-update.service
 [Unit]
