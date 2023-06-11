e1f0317346d6c869600a715532ceb880a817b8b7
smile <134200591+smileygames@users.noreply.github.com>
2023-06-11 22:22:11 +0900

    #81 とりあえずのたたき台

--- bin/ddns_timer/multi_domain.sh
+++ bin/ddns_timer/multi_domain.sh
@@ -2,15 +2,20 @@
 #
 # ./ddns_timer/multi_domain.sh
 #
-# MyDNS
+# multi_domain.sh
 
 Mode=$1
-Login_URL=$2
-IP_Version=$3
-DNS_Record=$4
+IP_Version=$2
+DNS_Record=$3
 
 # 配列のデータを読み込んでDDNSへアクセス
 multi_domain_ip_update() {
+    if [ "$IP_Version" = 4 ]; then
+        Login_URL=${MYDNS_IPV4_URL}
+    elif [ "$IP_Version" = 6 ]; then
+        Login_URL=${MYDNS_IPV6_URL}
+    fi
+
     for i in "${!MYDNS_ID[@]}"; do
         if [[ ${MYDNS_ID[$i]} = "" ]] || [[ ${MYDNS_PASS[$i]} = "" ]]; then
             ./err_message.sh "no_value" "${FUNCNAME[0]}" "MYDNS_ID[$i] or MYDNS_PASS[$i]"
@@ -22,6 +27,14 @@ multi_domain_ip_update() {
 
 # 配列のデータを読み込んでアドレスをチェックし変更があった場合のみ、DDNSへアクセス
 multi_domain_mydns_check() {
+    IP_New=$1
+
+    if [ "$IP_Version" = 4 ]; then
+        Login_URL=${MYDNS_IPV4_URL}
+    elif [ "$IP_Version" = 6 ]; then
+        Login_URL=${MYDNS_IPV6_URL}
+    fi
+
     for i in "${!MYDNS_ID[@]}"; do
         if [[ ${MYDNS_ID[$i]} = "" ]] || [[ ${MYDNS_PASS[$i]} = "" ]] || [[ ${MYDNS_DOMAIN[$i]} = "" ]]; then
             ./err_message.sh "no_value" "${FUNCNAME[0]}" "MYDNS_ID[$i] or MYDNS_PASS[$i] or MYDNS_DOMAIN[$i]"
@@ -35,18 +48,39 @@ multi_domain_mydns_check() {
     done
 }
 
+multi_domain_google_check() {
+    IP_New=$1
+
+    for i in "${!GOOGLE_ID[@]}"; do
+        if [[ ${GOOGLE_ID[$i]} = "" ]] || [[ ${GOOGLE_PASS[$i]} = "" ]] || [[ ${GOOGLE_DOMAIN[$i]} = "" ]]; then
+            ./err_message.sh "no_value" "${FUNCNAME[0]}" "GOOGLE_ID[$i] or GOOGLE_PASS[$i] or GOOGLE_DOMAIN[$i]"
+            continue
+        fi 
+        IP_old=$(dig "${GOOGLE_DOMAIN[i]}" "$DNS_Record" +short)  # ドメインのアドレスを読み込む
+
+        if [[ $IP_New != "$IP_old" ]]; then
+            ./dns_access.sh "google" "$i" "${GOOGLE_ID[$i]}:${GOOGLE_PASS[$i]} ${GOOGLE_URL}?hostname=${GOOGLE_DOMAIN}&myip=${IP_New}"
+        fi
+    done
+}
+
 # 実行スクリプト
 case ${Mode} in
    "update")
         multi_domain_ip_update
         ;;
    "check") 
-        IP_New=$(curl -s ifconfig.io -"$IP_Version")  # 自分のアドレスを読み込む
-        if [[ $IP_New = "" ]]; then
+        MyIP=$(curl -s ifconfig.io -"$IP_Version")  # 自分のアドレスを読み込む
+        if [[ $MyIP = "" ]]; then
             ./err_message.sh "no_value" "${FUNCNAME[0]}" "自分のIPアドレスを取得できなかった"
             return 1
         fi
-        multi_domain_mydns_check
+        if [ ${#MYDNS_ID[@]} != 0 ]; then
+            multi_domain_mydns_check "$MyIP"
+        fi
+        if [ ${#GOOGLE_ID[@]} != 0 ]; then
+            multi_domain_google_check "$MyIP"
+        fi       
         ;;
     * )
         echo "[${Mode}] <- 引数エラーです"
